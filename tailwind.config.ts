6ec59bec2127da32817bd2225c5f970215f300d6
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2023-11-13 00:20:08 +0300

    Changed the whole animations. Switched libraries from React-spring to Framer-motion.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,27 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      }
-      
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        background: "#040e1a",
+        text: "#D8CEE8",
+        primary: "#17df04",
+        secondary: "#d8cee8",
+        accent: "#FFA500",
+      },
     },
-    colors: {
-      "primary" : "#16FF00",
-      "background": "#040e1a",
-      "text": "#d8cee8",
-      "secondary" : "#d8cee8",
-      "accent": "#040e1a"
-    }
   },
   plugins: [],
-}
-export default config
+};
+export default config;

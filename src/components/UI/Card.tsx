6300d1e834b01676cfd82509aaa53c11f704e22b
Kutalmış Berke Yılmaz <kberkeyilmaz@gmail.com>
2023-11-25 14:09:09 +0300

    UPDATE: Refactored client components in home, added Button as a component. Home now runs from server.

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,9 +1,10 @@
 import { Props } from "@/libs/types";
 
-const cardClasses = "bg-text text-background p-5 rounded-md cursor-pointer";
+const cardClasses = "bg-text text-background p-5 rounded-md cursor-pointer overflow-hidden";
 
 const Card: React.FC<Props> = ({ children, className }) => {
   return <div className={`${cardClasses} ${className}`}>{children}</div>;
 };
 
 export default Card;
+
